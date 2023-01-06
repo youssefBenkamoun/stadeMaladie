df36bb4f5544a8c3fd951ab49e2a8cfe600f5fd1
HmzEzh <hamzaezzahi177@gmail.com>
2023-01-06 22:32:06 +0100

    auth2

--- src/main/java/com/example/stademaladie/repositories/UserRepository.java
+++ src/main/java/com/example/stademaladie/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.stademaladie.repositories;
 import com.example.stademaladie.beans.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
